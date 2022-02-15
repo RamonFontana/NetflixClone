e6525712d4560b3eb1fe721ff82c0e5692a5e200
RamonFonatana <fontanaramon@gmail.com>
2022-02-14 21:09:26 -0300

    criado configuração e conecção com TMDB

---
+++ src/Tmdb.js
@@ -0,0 +1,55 @@
+const API_KEY = 'f0597580befa139080998a03d431d899';
+const API_URL = 'https://api.themoviedb.org/3';
+
+const basicFetch = async (endpoint) => {
+    const req = await fetch(`${API_URL}/${endpoint}`);
+    const json = await req.json();
+    return json;
+}
+
+export default {
+    getHomeList: async () => {
+        return [
+            {
+                slug: 'originals',
+                title: "Originais da Netflix",
+                items: await basicFetch(`discover/tv?with_networks=213&language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'trending',
+                title: "Recomendados para Você",
+                items: await basicFetch(`trending/all/week?language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'toprated',
+                title: "Em Alta",
+                items: await basicFetch(`movie/top_rated?language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'action',
+                title: "Ação",
+                items: await basicFetch(`discover/movie?with_genres=28&language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'comedy',
+                title: "Comédia",
+                items: await basicFetch(`discover/movie?with_genres=35&language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'horror',
+                title: "Terror",
+                items: await basicFetch(`discover/movie?with_genres=27&language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'romance',
+                title: "Romance",
+                items: await basicFetch(`discover/movie?with_genres=10749&language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'documentary',
+                title: "Documentário",
+                items: await basicFetch(`discover/movie?with_genres=99&language=pt-BR&api_key=${API_KEY}`)
+            },
+        ]
+    }
+}
