851185ce0a54eabd43aa8bbabe039c24bc2e03da
RamonFonatana <fontanaramon@gmail.com>
2022-02-24 19:56:22 -0300

    Criado Featured

--- components/MovieRow.jsx
+++ components/MovieRow.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import style from "../styles/MovieRow.module.scss";
 
-export default (props) => {
-  console.log(props.items.results.length);
+export default function MovieRow({items, title}) {
   return (
     <div className={style.card}>
-      <h2>{props.title}</h2>
+      <h2>{title}</h2>
       <div className={style.slideMovies}>
-        {props.items.results.length > 0 &&
-          props.items.results.map((item, key) => (
-            <div className={style.slideItem}>
+        {items.results.length > 0 &&
+          items.results.map((item, key) => (
+            <div key={key} className={style.slideItem}>
               <img
                 src={`https://image.tmdb.org/t/p/w300${item.poster_path}`}
                 alt={item.original_title}
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,31 @@
-import React, { useEffect, useState } from 'react';
-import Head from 'next/head';
-import Tmdb from '../src/Tmdb';
-import style from '../styles/Home.module.scss';
-import MovieRow from '../components/MovieRow'
+import React, { useEffect, useState } from "react";
+import Head from "next/head";
+import Tmdb from "../src/Tmdb";
+import style from "../styles/Home.module.scss";
+import MovieRow from "../components/MovieRow";
+import FeaturedMovie from "../components/FeaturedMovie";
 
 export default function Home() {
   const [movieList, setMovieList] = useState([]);
+  const [featured, setFeatured] = useState(false);
 
   useEffect(() => {
     const loadAll = async () => {
       let list = await Tmdb.getHomeList();
       setMovieList(list);
-    }
+
+      let originals = list.filter((i) => i.slug === "originals");
+      let randomChosen = Math.floor(
+        Math.random() * (originals[0].items.results.length - 1)
+      );
+      let chosen = originals[0].items.results[randomChosen];
+      let chosenInfo = await Tmdb.getFeatured(chosen.id, "tv");
+      setFeatured(chosenInfo);
+    };
 
     loadAll();
-  }, [])
+    console.log(featured);
+  }, []);
 
   return (
     <div className={style.page}>
@@ -23,10 +34,11 @@ export default function Home() {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <div className={style.lists}>
-      { movieList.map((item, key) => (
+        {featured && <FeaturedMovie item={featured} />}
+        {movieList.map((item, key) => (
           <MovieRow key={key} title={item.title} items={item.items} />
-      )) }
+        ))}
       </div>
     </div>
-  )
+  );
 }
